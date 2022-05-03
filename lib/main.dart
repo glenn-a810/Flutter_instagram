15c11dc975cf33d4df419c42f230eb8fcd378be6
Glenn <a810.glenn@gmail.com>
2022-05-03 23:46:54 +0900

    새 페이지 전환 방법

--- lib/main.dart
+++ lib/main.dart
@@ -264,13 +264,20 @@ class _HomeState extends State<Home> {
                       context,
                       // CupertinoPageRoute(builder: (c) => Profile()),
                       PageRouteBuilder(
-                          pageBuilder: (context, a1, a2) => Profile(),
-                          transitionsBuilder: (context, a1, a2, child) =>
-                              FadeTransition(
-                                opacity: a1,
-                                child: child,
-                              ) // animation widget
-                          ));
+                        pageBuilder: (context, a1, a2) => Profile(),
+                        transitionsBuilder: (context, a1, a2, child) =>
+                            SlideTransition(
+                          position: Tween(
+                            begin: Offset(-1.0, 0.0),
+                            end: Offset(0.0, 0.0),
+                          ).animate(a1),
+                          child: child,
+                        ),
+                        //     FadeTransition(
+                        //         opacity: a1, child: child), // animation widget
+                        // transitionDuration:
+                        //     Duration(milliseconds: 500), // animation 속도
+                      ));
                 },
               ),
               Text('좋아요 ' + widget.resData[i]['likes'].toString()),
